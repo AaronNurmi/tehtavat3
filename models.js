f829ffd4fb19c9599aa49b49a28a2b6ad4ed4fe0
AaronNurmi <144720744+AaronNurmi@users.noreply.github.com>
2025-10-31 03:07:51 +0200

    Add files via upload
    3.13 Tietokantamäärittelyjen eriyttäminen moduuliksi

---
+++ models.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  number: { type: String, required: true }
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Person = mongoose.model('Person', personSchema)
+export default Person
